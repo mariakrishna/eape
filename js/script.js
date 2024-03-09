d62a4f63c6edc8635de2d01cbf29495c5f247f91
mariakrishna <mkadonai@gmail.com>
2024-03-09 00:47:05 -0300

    sections finished

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
-  speed: 400,
-  spaceBetween: 100,
-
+  spaceBetween: 10,
+  slidesPerView: 3,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
