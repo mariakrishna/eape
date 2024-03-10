07ce3626bd6d909e06083cef6e8515f1c1b9029f
mariakrishna <mkadonai@gmail.com>
2024-03-09 23:20:40 -0300

    finished

--- js/script.js
+++ js/script.js
@@ -5,5 +5,25 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
 
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    }
+  }
 });
